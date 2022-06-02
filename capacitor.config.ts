38180a386737e77ca296781f68cb2ec6d5068de9
Joe Mainwaring <joseph.mainwaring@gmail.com>
2022-06-01 22:31:53 -0500

    add capicator.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.mainwaring.pwa',
+  appName: 'Mainwaring.dev',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
